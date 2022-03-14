205febb08f19ef16dec7cee041afe51374170058
Siks6 <ingaketsiweingaketsiwe@gmail.com>
2022-03-14 14:16:54 -0700

    lets print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * prints numbers from 0 up to 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+	}
+
+	putchar('\n');
+	return (0);
+}
