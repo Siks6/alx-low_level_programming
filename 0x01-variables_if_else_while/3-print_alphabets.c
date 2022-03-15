b675b6f8c957de69b52786d284165e89868c8ec3
Siks6 <ingaketsiweingaketsiwe@gmail.com>
2022-03-14 18:41:00 -0700

    alphBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry
+ *
+ * prints alphabet first in lower and then in uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; x++)
+	{
+		putchar(tolower(x));
+	}
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar(toupper(x));
+	}
+	putchar('\n');
+	return (0);
+}
