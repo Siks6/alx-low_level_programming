9c73d9af1b685b0b38af2da170e158788d6a32b1
Siks6 <ingaketsiweingaketsiwe@gmail.com>
2022-03-15 15:53:11 -0700

    my header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+/**
+ * making header file for prototypes of functions
+ *
+ */
+
+void _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int n);
+
+#endif // MAIN_H_
