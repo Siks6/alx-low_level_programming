eceacfcb9d013e7255f3fe055ddf1236756100a2
Siks6 <ingaketsiweingaketsiwe@gmail.com>
2022-03-16 06:52:39 -0700

    first task in nedted loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * writes out _putchar to stdout, followed by a new line
+ *
+ * Return always: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char word[8] = "_putchar\n";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+	return (0);
+}
