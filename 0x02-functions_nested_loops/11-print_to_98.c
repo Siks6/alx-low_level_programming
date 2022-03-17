eac70f68e43e87b6c5260db1a903e618861f1b9c
Siks6 <ingaketsiweingaketsiwe@gmail.com>
2022-03-16 17:12:38 -0700

    lets commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -19,7 +19,7 @@ void print_to_98(int n)
 		if (n == 98)
 
 			continue;
-		printf(",");
+		printf(", ");
 		}
 	printf("\n");
 	}
@@ -31,7 +31,7 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-			printf(",");
+			printf(", ");
 		}
 	}
 }
